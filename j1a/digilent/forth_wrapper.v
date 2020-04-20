e11eb255098c453262557767f4b035041f22a56e
Andy Rabagliati <andyr@wizzy.com>
2020-04-20 22:16:03 +0200

    memory now initialises

--- j1a/digilent/forth_wrapper.v
+++ j1a/digilent/forth_wrapper.v
@@ -4,6 +4,7 @@
 
 module forth_wrapper
 #(
+    parameter MEM_INIT_FILE = "/home/andyr/git/vivado/src/swapforth/j1a/digilent/nuc.mcs",
     parameter LOG2ABITS=11,
     parameter DWIDTH = 16
 )
@@ -15,32 +16,29 @@ reg clk, reset;
 wire io_rd, io_wr;
 wire [DWIDTH-1:0] addra, addrb;
 wire wea, web;
-wire [DWIDTH-1:0] reada, readb;
+reg [DWIDTH-1:0] reada, readb;
 wire [DWIDTH-1:0] writea, writeb;
 
-ram_dual
-#(
-    .MEM_INIT_FILE("/home/andyr/git/vivado/src/swapforth/j1a/digilent/nuc.mcs"),
-    .LOG2ABITS(LOG2ABITS),
-    .DWIDTH(DWIDTH)
-) ram (
-    .addra(addra),      .addrb(addrb),
-    .reada(reada),      .readb(readb),
-    .writea(writea),    .writeb(writeb),
-    .wea(wea),          .web(web),
-    .clk(clk)
-);
+(* ram_init_file = "nuc.mcs" *) reg [DWIDTH-1:0] mem [0:(1<<LOG2ABITS)-1];
 
-j1 _j1(
-    .mem_wr(web),
-    .dout(writeb),
-    .mem_addr(addrb),
-    .io_din(readb),
-    .code_addr(addra),
-    .insn(reada),
-    .clk(clk),
-    .resetq(reset)
-);
+initial begin
+    if (MEM_INIT_FILE != "") begin
+        $readmemh(MEM_INIT_FILE, mem);
+    end
+end
+
+always @(posedge clk) begin
+    if (wea)
+        mem[addra] <= writea;
+    else
+        reada <= mem[addra];
+end
+always @(posedge clk) begin
+    if (web)
+        mem[addrb] <= writeb;
+    else
+        readb <= mem[addrb];
+end
 
 always #5 clk = (clk === 1'b0);
 
